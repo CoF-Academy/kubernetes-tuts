56295fc323c7d6fbd1b8432a79ecdb4ef36e7880
Jorge Reus <j-g1996@live.com>
2020-08-10 20:01:09 -0500

    Implemented loadbalancing of rooms in socketio

--- signaling-server/src/index.ts
+++ signaling-server/src/index.ts
@@ -1,4 +1,4 @@
-import { loadbalance } from  './loadbalancer/loadbalancer'
+import { LoadBalancer } from  './loadbalancer/loadbalancer'
 import * as http from "http"
 import ioserver, { Socket } from 'socket.io';
 
@@ -9,7 +9,7 @@ const io = ioserver(server);
 // This is only to test loadbalancing
 let room_names = ['grupo_1', 'grupo_2', 'grupo_3'];
 
-let loadbalancer = loadbalance(room_names, 30)
+let loadbalancer = new LoadBalancer(room_names, 30)
 
 // Authenticate the user
 io.use((socket, next) => {
@@ -19,16 +19,18 @@ io.use((socket, next) => {
 });
 
 io.on('connection', client => {
-  let room_id = loadbalancer();
-    let client_rooms = Object.keys(client.adapter.sids[client.id]);
-    console.log(client_rooms);
-
+  let room_id = loadbalancer.getGroup();
+  client.join(room_id.toString());
   io.to(client.id).emit('message', `Hola ${client.id}, bienvenido al room ${room_id}`);
+  
+  console.log(loadbalancer.getAllRoomsCopy());
   client.on('disconnecting', () => {
-    // console.log(`Client ${client.id} disconnected`);
+    let client_rooms = Object.keys(client.adapter.sids[client.id]);
+    client_rooms.shift();
+    client_rooms.forEach(room_name => loadbalancer.incrementByName(room_name));
+    console.log(loadbalancer.getAllRoomsCopy());
+    console.log(`Client ${client.id} disconnected`);
   });
 });
 
 server.listen(3000);
-
-
